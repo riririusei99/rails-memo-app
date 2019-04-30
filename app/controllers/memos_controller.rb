2b560eb175954cf9a7c1b230488f6b11a33369a5
riririusei99 <riry1313@gmail.com>
2019-04-30 03:31:50 +0000

    add create action

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -3,4 +3,14 @@ class MemosController < ApplicationController
     @memos = Memo.all
     @memo = Memo.new
   end 
+
+  def create
+    Memo.create(memo_params)
+    redirect_to :root
+  end
+  
+  private
+  def memo_params
+    params.require(:memo).permit(:text)
+  end
 end
